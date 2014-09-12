0432e6041651b59cc28efd56a8c850b39e0fa899
Haikuan Bao <haibao@cisco.com>
2014-09-12 22:13:01 +0800

    delete extra lines

--- js/directive.js
+++ js/directive.js
@@ -2,15 +2,10 @@ signUpModule.directive('ngModelOnenter', function() {
     return {
       restrict: 'A',
       require: 'ngModel',
-      priority: 1, // needed for angular 1.2.x
+      priority: 1,
       link: function(scope, elm, attr, ngModelCtrl) {
         if (attr.type === 'radio' || attr.type === 'checkbox') return;
         elm.unbind('input').unbind('keydown').unbind('change');
-        /*elm.bind('blur', function() {
-          scope.$apply(function() {
-            ngModelCtrl.$setViewValue(elm.val());
-          });
-        });*/
         scope.userinfo_elm[elm.attr('name')] = elm;
         elm.bind("keydown keypress", function(event) {
           if (event.which === 13) {
